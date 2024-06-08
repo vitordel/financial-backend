dd302fcc637e2f81f2e1d9f944bef7cea70d1625
Vitor Delgado <vitor.ldelgado@gmail.com>
2024-06-08 17:07:29 -0300

    first commit

---
+++ src/main/java/br/com/vitordel/financial_backend/FinancialBackendApplication.java
@@ -0,0 +1,13 @@
+package br.com.vitordel.financial_backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FinancialBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FinancialBackendApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/vitordel/financial_backend/FinancialBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.vitordel.financial_backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FinancialBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
